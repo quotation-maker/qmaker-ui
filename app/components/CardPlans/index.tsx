98b0f573e01b96303938c1ac2259ec4c93df39fa
hyunwlee <hwlee3928@gmail.com>
2024-09-21 17:10:37 +0900

    feat: card-plans component, css, motion 구현

---
+++ app/components/CardPlans/index.tsx
@@ -0,0 +1,105 @@
+'use client';
+
+import { Card, CardContent, Typography } from '@mui/material';
+import clsx from 'clsx';
+import { motion } from 'framer-motion';
+import styles from './cardPlans.css';
+import { inVariants, outVariants } from './motion';
+
+interface ICustomCard {
+  variant: 'primary' | 'secondary';
+  subTitle: string;
+  subContent: string;
+  benefits: string[];
+  price: string;
+  dateCycleUnit?: 'month' | 'year';
+}
+
+function CustomCard({
+  variant,
+  subTitle,
+  subContent,
+  benefits,
+  price,
+  dateCycleUnit,
+}: ICustomCard) {
+  return (
+    <motion.div initial="hidden" whileInView="show" variants={outVariants}>
+      <Card className={clsx(styles.card, styles[variant] as string)}>
+        <CardContent className={styles.cardContent}>
+          <Typography className={styles.subTitle} variant="subtitle1">
+            {subTitle}
+          </Typography>
+          <Typography className={styles.subContent} variant="body1">
+            {subContent}
+          </Typography>
+          <Typography
+            className={clsx(styles.price, {
+              [styles.bright]: variant === 'primary',
+            })}
+            variant="h3"
+          >
+            {price}
+            <span>{dateCycleUnit && ` / ${dateCycleUnit}`}</span>
+          </Typography>
+          <ul className={styles.benefitList}>
+            {benefits.map(benefit => (
+              <motion.li
+                key={benefit}
+                className={styles.benefit}
+                variants={inVariants}
+              >
+                <motion.svg className={styles.checkIcon}>
+                  <motion.path
+                    d="M10.319.768l1.363 1.464-7.128 6.634L.319 4.924 1.682 3.46l2.871 2.674z"
+                    width={12}
+                    height={9}
+                    fill={variant === 'primary' ? '#CDF348' : '#243847'}
+                  />
+                </motion.svg>
+                <Typography className={styles.benefitContent} variant="body1">
+                  {benefit}
+                </Typography>
+              </motion.li>
+            ))}
+          </ul>
+        </CardContent>
+      </Card>
+    </motion.div>
+  );
+}
+
+export default function CardPlans() {
+  return (
+    <section className={styles.container}>
+      <h2 className={styles.heading}>our pricing plans</h2>
+      <div className={styles.cardWrapper}>
+        <CustomCard
+          variant="secondary"
+          subTitle="Dip your toe"
+          subContent="Just getting started? No problem at all! Our free plan will take you a long way."
+          price="Free"
+          benefits={[
+            'Unlimited products',
+            'Basic analytics',
+            'Limited marketplace exposure',
+            '10% fee per transaction',
+          ]}
+        />
+        <CustomCard
+          variant="primary"
+          subTitle="Dive right in"
+          subContent="Ready for the big time? Our paid plan will help you take your business to the next level."
+          price="$25.00"
+          dateCycleUnit="month"
+          benefits={[
+            'Custom domain',
+            'Advanced analytics and reports',
+            'High marketplace visibility',
+            '5% fee per transaction',
+          ]}
+        />
+      </div>
+    </section>
+  );
+}
